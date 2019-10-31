dd7ce6fe483331e5691cced2938ba849c8fcc225
mmetzgerdpu2021 <44932633+mmetzgerdpu2021@users.noreply.github.com>
2019-10-31 13:10:29 -0500

    Filtering.

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 var elements = document.getElementsByTagName('*');
+var oldWords = [];
 var oldWord;
 var newWord;
 
@@ -11,41 +12,43 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var oldWord = 'impeachment';  
+            if (text.length > 6) {
+                oldWords.push(text);            
+            }
+            oldWord = "radioactive";
         }
     }
 }
 
-console.log(`BEFORE CALL: ${newWord}`);
-
 // API call to get synonyms for words to replace
 for (var i = 0; i < 1; i++){
     const request = async () => {
-        const response = await fetch(`https://api.datamuse.com/words?ml=impeachment`);
+        const response = await fetch(`https://api.datamuse.com/words?ml=radioactive`);
         const json = await response.json();
-        console.log(json);
-        newWord = json[0].word;
+        newWord = json[3].word;
+
+        console.log(oldWords);
         console.log(newWord);
     }
     request();
 }
 
-console.log(`AFTER CALL: ${newWord}`)
+setTimeout(function(){
+    // Actually replace all words
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
 
-// Actually replace all words
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+        for (var j = 0; j < element.childNodes.length; j++) {
+            var node = element.childNodes[j];
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(oldWord, newWord);
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
+            if (node.nodeType === 3) {
+                var text = node.nodeValue;
+                var replacedText = text.replace(oldWord, newWord);
+                if (replacedText !== text) {
+                    element.replaceChild(document.createTextNode(replacedText), node);
+                }
+                        
             }
-                    
         }
     }
-}
+}, 1000)
