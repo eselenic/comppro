9a476e72b71bc5ced725dd5672aef88c01c06b58
mmetzgerdpu2021 <44932633+mmetzgerdpu2021@users.noreply.github.com>
2019-10-31 14:46:09 -0500

    Read and add to lists.

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 var elements = document.getElementsByTagName('*');
-var oldWords = [];
+var oldWords = ["radioactive", "residents", "community"];
 var oldWord;
+var newWords = [];
 var newWord;
 
 // Find all the words to replace
@@ -12,42 +13,42 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            if (text.length > 6) {
-                oldWords.push(text);            
-            }
+            // if (text.length > 6) {
+            //     oldWords.push(text);            
+            // }
             oldWord = "radioactive";
         }
     }
 }
 
 // API call to get synonyms for words to replace
-for (var i = 0; i < 1; i++){
+for (var i = 0; i < oldWords.length; i++){
     const request = async () => {
-        const response = await fetch(`https://api.datamuse.com/words?ml=radioactive`);
+        const response = await fetch(`https://api.datamuse.com/words?ml=${oldWords[i]}`);
         const json = await response.json();
-        newWord = json[3].word;
-
-        console.log(oldWords);
-        console.log(newWord);
+        newWord = json[2].word;
+        newWords.push(newWord);
     }
     request();
 }
 
 setTimeout(function(){
     // Actually replace all words
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
+    for (var k = 0; k < newWords.length; k++){
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
 
-        for (var j = 0; j < element.childNodes.length; j++) {
-            var node = element.childNodes[j];
+            for (var j = 0; j < element.childNodes.length; j++) {
+                var node = element.childNodes[j];
 
-            if (node.nodeType === 3) {
-                var text = node.nodeValue;
-                var replacedText = text.replace(oldWord, newWord);
-                if (replacedText !== text) {
-                    element.replaceChild(document.createTextNode(replacedText), node);
+                if (node.nodeType === 3) {
+                    var text = node.nodeValue;
+                    var replacedText = text.replace(oldWords[k], newWords[k]);
+                    console.log(oldWords[k] + " " + newWords[k]);
+                    if (replacedText !== text) {
+                        element.replaceChild(document.createTextNode(replacedText), node);
+                    }        
                 }
-                        
             }
         }
     }
